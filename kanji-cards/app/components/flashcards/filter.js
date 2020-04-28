a3e11087b00f4d87da6ac38bf047a9647cc6a05e
kcbobbe <kcbobbe@gmail.com>
2020-04-28 14:45:16 -0400

    implemented search bar

---
+++ kanji-cards/app/components/flashcards/filter.js
@@ -0,0 +1,22 @@
+import Component from '@glimmer/component';
+
+export default class FlashcardsFilterComponent extends Component {
+  get results() {
+    let { kanjis, query } = this.args;
+
+    if (query) {
+
+      console.log(query)
+      kanjis = kanjis.filter(kanji => (kanji.kanji.includes(query)
+      // || kanji.kmeaning.includes(query)
+      || kanji.kunyomi_ja.includes(query)
+      || kanji.onyomi_ja.includes(query)
+      // || kanji.name.includes(query)
+      || kanji.kunyomi.includes(query)
+      || kanji.onyomi.includes(query)
+      ));
+    }
+
+    return kanjis;
+  }
+}
